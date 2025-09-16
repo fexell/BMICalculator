4b04d168716f0a1bd097b5de20ba4660f3770fde
Felix Cervin <fexellofficial@gmail.com>
2025-09-16 15:08:26 +0200

    feat: Weight- and height validator helpers

---
+++ BMICalculator/Helpers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculator {
+    internal class Helpers {
+        public static double WeightValidator( string value, string unit = "metric" ) {
+            while ( true ) {
+                if ( unit == "metric" ) {
+                    if ( double.TryParse( value, out var weight ) && weight > 40 && weight < 200 ) {
+                        return weight;
+                    } else {
+                        Console.Write( "Please enter a valid weight (40 - 200): " );
+                        value = Console.ReadLine();
+                    }
+                } else if( unit == "imperial" ) {
+                    if ( double.TryParse( value, out var weight ) && weight > 88 && weight < 440 ) {
+                        return weight;
+                    } else {
+                        Console.Write( "Please enter a valid weight (88 - 440): " );
+                        value = Console.ReadLine();
+                    }
+                }
+            }
+        }
+
+        public static double HeightValidator( string value ) {
+            while ( true ) {
+                if ( double.TryParse( value, out double height ) && height > 1.20 && height < 2.00 ) {
+                    return height;
+                } else {
+                    Console.Write( "Please enter a valid height (1.20 - 2.00): " );
+                    value = Console.ReadLine();
+                }
+            }
+        }
+    }
+}
--- BMICalculator/Program.cs
+++ BMICalculator/Program.cs
@@ -2,6 +2,19 @@
 
 namespace BMICalculator {
     internal class Program {
+        static double CalculateBMI( double weight, double height, string unit = "metric" ) {
+            if( unit == "metric" )
+                return weight / ( height * height );
+
+            else if( unit == "imperial" )
+                return 703 * weight / ( height * height );
+
+            else {
+                Console.WriteLine( "Invalid unit, returning 0." );
+                return 0;
+            }
+        }
+
         static void Main(string[] args) {
 
         }
